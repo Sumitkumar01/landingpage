9b4e21e61b67b0c91cde90b28b10a7ff65d95a5b
sumit <sk7627577@gmail.com>
2024-12-19 16:08:07 +0530

    refactor: enhance Container and Section components with additional props and create SectionWithContainer component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,19 @@
-const Container: React.FC<{ className: string }> = ({ className }) => {
-  return <div className={`max-width ${className}`}>Enter</div>;
+interface ContainerProps {
+  className?: string;
+  children: React.ReactNode;
+  id: string;
+}
+
+const Container: React.FC<ContainerProps> = ({
+  className = "",
+  children,
+  id,
+}) => {
+  return (
+    <div className={`max-width ${className}`} id={id}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,12 +3,20 @@ interface SectionProps {
   lgpy?: number;
   py?: number;
   children: React.ReactNode;
+  id: string;
 }
 
-const Section: React.FC<SectionProps> = ({ className, lgpy, py, children }) => {
+const Section: React.FC<SectionProps> = ({
+  className = "",
+  lgpy,
+  py,
+  id,
+  children,
+}) => {
   return (
     <section
       className={`max-w-[1920px] mx-auto w-full ${lgpy ? `lg:py-${lgpy}` : "lg:py-10"} ${py ? `py-${py}` : "py-5"} ${className}`}
+      id={id}
     >
       {children}
     </section>

a8ce00e918da67bf94b359b8028ccd741c471edb
sumit <sk7627577@gmail.com>
2024-12-19 15:45:50 +0530

    initialize Next.js project with essential configurations and components

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="max-width font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <p className="text-lg">This is a Geist UI demo.</p>
+    </div>
+  );
+}
